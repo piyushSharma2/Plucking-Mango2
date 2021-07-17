93bedb0ae648195de570dba2cca7b73e315ddb74
piyushSharma2 <76560083+piyushSharma2@users.noreply.github.com>
2021-07-17 16:20:52 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,28 @@
+class Stone{
+
+    constructor(x,y,r){
+        var Options={
+            isStatic:false,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body=Bodies.circle(this.x,this.y,this.r/2,Options)
+        this.image=loadImage("images/stone.png")
+        World.add(world,this.body)
+    }
+    display(){
+        push()
+        translate(this.body.position.x,this.body.position.y)
+        angleMode(RADIANS)
+       // rotate(this.body.angle)
+        imageMode(CENTER)
+        image(this.image,0,0,this.r*2,this.r*2)
+        pop()
+    }
+
+}
